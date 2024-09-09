013ac08e9175466062a7d7dcc194a71847092ddc
HAHOOS <gracjan.dwornicki@gmail.com>
2024-09-09 20:58:40 +0200

    Added timeout after Rate Limit Response

--- MelonAutoUpdater/Search/Included/Thunderstore.cs
+++ MelonAutoUpdater/Search/Included/Thunderstore.cs
@@ -1,6 +1,9 @@
-﻿using MelonLoader.TinyJSON;
+﻿using MelonAutoUpdater.Helper;
+using MelonLoader.TinyJSON;
 using Semver;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -16,6 +19,9 @@ namespace MelonAutoUpdater.Search.Included
         public override string Author => "HAHOOS";
 
         public override string Link => "https://thunderstore.io";
+
+        private bool disableAPI = false;
+        private long apiReset;
 
         public override Task<ModData> Search(string url, SemVersion currentVersion)
         {
@@ -37,72 +43,81 @@ namespace MelonAutoUpdater.Search.Included
                 }
                 HttpClient request = new HttpClient();
                 request.DefaultRequestHeaders.Add("User-Agent", UserAgent);
-                Task<HttpResponseMessage> response = request.GetAsync($"https://thunderstore.io/api/experimental/package/{namespaceName}/{packageName}/");
-                response.Wait();
-                if (response.Result.IsSuccessStatusCode)
+                if (disableAPI && DateTimeOffset.UtcNow.ToUnixTimeSeconds() > apiReset) disableAPI = false;
+                if (!disableAPI)
                 {
-                    Task<string> body = response.Result.Content.ReadAsStringAsync();
-                    body.Wait();
-                    if (body.Result != null)
+                    Task<HttpResponseMessage> response = request.GetAsync($"https://thunderstore.io/api/experimental/package/{namespaceName}/{packageName}/");
+                    response.Wait();
+                    if (response.Result.IsSuccessStatusCode)
                     {
-                        var _data = JSON.Load(body.Result);
-
-                        request.Dispose();
-                        response.Dispose();
-                        body.Dispose();
-
-                        List<FileData> files = new List<FileData>();
-
-                        FileData fileData = new FileData
+                        Task<string> body = response.Result.Content.ReadAsStringAsync();
+                        body.Wait();
+                        if (body.Result != null)
                         {
-                            FileName = packageName,
-                            URL = (string)_data["latest"]["download_url"]
-                        };
+                            var _data = JSON.Load(body.Result);
 
-                        files.Add(fileData);
+                            request.Dispose();
+                            response.Dispose();
+                            body.Dispose();
 
-                        bool isSemVerSuccess = SemVersion.TryParse((string)_data["latest"]["version_number"], out SemVersion semver);
-                        if (!isSemVerSuccess)
-                        {
-                            Logger.Error($"Failed to parse version");
-                            return ReturnEmpty();
+                            List<FileData> files = new List<FileData>();
+
+                            FileData fileData = new FileData
+                            {
+                                FileName = packageName,
+                                URL = (string)_data["latest"]["download_url"]
+                            };
+
+                            files.Add(fileData);
+
+                            bool isSemVerSuccess = SemVersion.TryParse((string)_data["latest"]["version_number"], out SemVersion semver);
+                            if (!isSemVerSuccess)
+                            {
+                                Logger.Error($"Failed to parse version");
+                                return null;
+                            }
+
+                            return Task.Factory.StartNew<ModData>(() => new ModData()
+                            {
+                                LatestVersion = semver,
+                                DownloadFiles = files,
+                            });
                         }
-
-                        return Task.Factory.StartNew<ModData>(() => new ModData()
+                        else
                         {
-                            LatestVersion = semver,
-                            DownloadFiles = files,
-                        });
+                            Logger.Error("Thunderstore API returned no body, unable to fetch package information");
+
+                            request.Dispose();
+                            response.Dispose();
+                            body.Dispose();
+
+                            return null;
+                        }
                     }
                     else
                     {
-                        Logger.Error("Thunderstore API returned no body, unable to fetch package information");
-
+                        if (response.Result.StatusCode == System.Net.HttpStatusCode.NotFound)
+                        {
+                            Logger.Warning("Thunderstore API could not locate the mod/plugin");
+                        }
+                        else if (response.Result.StatusCode == System.Net.HttpStatusCode.Forbidden || (int)response.Result.StatusCode == 429)
+                        {
+                            disableAPI = true;
+                            apiReset = DateTimeOffset.Now.AddMinutes(1).ToUnixTimeSeconds();
+                        }
+                        else
+                        {
+                            Logger.Error
+                                ($"Failed to fetch package information from Thunderstore, returned {response.Result.StatusCode} with following message:\n{response.Result.ReasonPhrase}");
+                        }
                         request.Dispose();
                         response.Dispose();
-                        body.Dispose();
 
-                        return ReturnEmpty();
+                        return null;
                     }
-                }
-                else
-                {
-                    if (response.Result.StatusCode == System.Net.HttpStatusCode.NotFound)
-                    {
-                        Logger.Warning("Thunderstore API could not locate the mod/plugin");
-                    }
-                    else
-                    {
-                        Logger.Error
-                            ($"Failed to fetch package information from Thunderstore, returned {response.Result.StatusCode} with following message:\n{response.Result.ReasonPhrase}");
-                    }
-                    request.Dispose();
-                    response.Dispose();
-
-                    return ReturnEmpty();
                 }
             }
-            return ReturnEmpty();
+            return null;
         }
     }
 }
