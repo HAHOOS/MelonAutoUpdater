ee942f6a4d75268244bf060acdc635342180e525
HAHOOS <gracjan.dwornicki@gmail.com>
2024-09-10 17:19:20 +0200

    Add Brute Checking

--- MelonAutoUpdater/Search/Included/Thunderstore.cs
+++ MelonAutoUpdater/Search/Included/Thunderstore.cs
@@ -3,7 +3,6 @@ using MelonLoader.TinyJSON;
 using Semver;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -20,8 +19,90 @@ namespace MelonAutoUpdater.Search.Included
 
         public override string Link => "https://thunderstore.io";
 
+        public override bool BruteCheckEnabled => true;
+
         private bool disableAPI = false;
         private long apiReset;
+
+        internal Task<ModData> Check(string packageName, string namespaceName)
+        {
+            HttpClient request = new HttpClient();
+            request.DefaultRequestHeaders.Add("User-Agent", UserAgent);
+            if (disableAPI && DateTimeOffset.UtcNow.ToUnixTimeSeconds() > apiReset) disableAPI = false;
+            if (!disableAPI)
+            {
+                Task<HttpResponseMessage> response = request.GetAsync($"https://thunderstore.io/api/experimental/package/{namespaceName}/{packageName}/");
+                response.Wait();
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    Task<string> body = response.Result.Content.ReadAsStringAsync();
+                    body.Wait();
+                    if (body.Result != null)
+                    {
+                        var _data = JSON.Load(body.Result);
+
+                        request.Dispose();
+                        response.Dispose();
+                        body.Dispose();
+
+                        List<FileData> files = new List<FileData>();
+
+                        FileData fileData = new FileData
+                        {
+                            FileName = packageName,
+                            URL = (string)_data["latest"]["download_url"]
+                        };
+
+                        files.Add(fileData);
+
+                        bool isSemVerSuccess = SemVersion.TryParse((string)_data["latest"]["version_number"], out SemVersion semver);
+                        if (!isSemVerSuccess)
+                        {
+                            Logger.Error($"Failed to parse version");
+                            return null;
+                        }
+
+                        return Task.Factory.StartNew<ModData>(() => new ModData()
+                        {
+                            LatestVersion = semver,
+                            DownloadFiles = files,
+                        });
+                    }
+                    else
+                    {
+                        Logger.Error("Thunderstore API returned no body, unable to fetch package information");
+
+                        request.Dispose();
+                        response.Dispose();
+                        body.Dispose();
+
+                        return null;
+                    }
+                }
+                else
+                {
+                    if (response.Result.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        Logger.Warning("Thunderstore API could not locate the mod/plugin");
+                    }
+                    else if (response.Result.StatusCode == System.Net.HttpStatusCode.Forbidden || (int)response.Result.StatusCode == 429)
+                    {
+                        disableAPI = true;
+                        apiReset = DateTimeOffset.Now.AddMinutes(1).ToUnixTimeSeconds();
+                    }
+                    else
+                    {
+                        Logger.Error
+                            ($"Failed to fetch package information from Thunderstore, returned {response.Result.StatusCode} with following message:\n{response.Result.ReasonPhrase}");
+                    }
+                    request.Dispose();
+                    response.Dispose();
+
+                    return null;
+                }
+            }
+            return null;
+        }
 
         public override Task<ModData> Search(string url, SemVersion currentVersion)
         {
@@ -41,83 +122,14 @@ namespace MelonAutoUpdater.Search.Included
                     packageName = split[split.Length - 1];
                     namespaceName = split[split.Length - 2];
                 }
-                HttpClient request = new HttpClient();
-                request.DefaultRequestHeaders.Add("User-Agent", UserAgent);
-                if (disableAPI && DateTimeOffset.UtcNow.ToUnixTimeSeconds() > apiReset) disableAPI = false;
-                if (!disableAPI)
-                {
-                    Task<HttpResponseMessage> response = request.GetAsync($"https://thunderstore.io/api/experimental/package/{namespaceName}/{packageName}/");
-                    response.Wait();
-                    if (response.Result.IsSuccessStatusCode)
-                    {
-                        Task<string> body = response.Result.Content.ReadAsStringAsync();
-                        body.Wait();
-                        if (body.Result != null)
-                        {
-                            var _data = JSON.Load(body.Result);
-
-                            request.Dispose();
-                            response.Dispose();
-                            body.Dispose();
-
-                            List<FileData> files = new List<FileData>();
-
-                            FileData fileData = new FileData
-                            {
-                                FileName = packageName,
-                                URL = (string)_data["latest"]["download_url"]
-                            };
-
-                            files.Add(fileData);
-
-                            bool isSemVerSuccess = SemVersion.TryParse((string)_data["latest"]["version_number"], out SemVersion semver);
-                            if (!isSemVerSuccess)
-                            {
-                                Logger.Error($"Failed to parse version");
-                                return null;
-                            }
-
-                            return Task.Factory.StartNew<ModData>(() => new ModData()
-                            {
-                                LatestVersion = semver,
-                                DownloadFiles = files,
-                            });
-                        }
-                        else
-                        {
-                            Logger.Error("Thunderstore API returned no body, unable to fetch package information");
-
-                            request.Dispose();
-                            response.Dispose();
-                            body.Dispose();
-
-                            return null;
-                        }
-                    }
-                    else
-                    {
-                        if (response.Result.StatusCode == System.Net.HttpStatusCode.NotFound)
-                        {
-                            Logger.Warning("Thunderstore API could not locate the mod/plugin");
-                        }
-                        else if (response.Result.StatusCode == System.Net.HttpStatusCode.Forbidden || (int)response.Result.StatusCode == 429)
-                        {
-                            disableAPI = true;
-                            apiReset = DateTimeOffset.Now.AddMinutes(1).ToUnixTimeSeconds();
-                        }
-                        else
-                        {
-                            Logger.Error
-                                ($"Failed to fetch package information from Thunderstore, returned {response.Result.StatusCode} with following message:\n{response.Result.ReasonPhrase}");
-                        }
-                        request.Dispose();
-                        response.Dispose();
-
-                        return null;
-                    }
-                }
+                return Check(packageName, namespaceName);
             }
             return null;
+        }
+
+        public override Task<ModData> BruteCheck(string name, string author, SemVersion currentVersion)
+        {
+            return Check(name, author);
         }
     }
 }
