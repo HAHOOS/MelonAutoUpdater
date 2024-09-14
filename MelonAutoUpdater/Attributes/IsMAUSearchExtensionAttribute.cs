59c17176ca5a228e85f32723bf0c537aa8863592
HAHOOS <gracjan.dwornicki@gmail.com>
2024-09-14 13:56:46 +0200

    Add new attributes to use for plugins/mods

---
+++ MelonAutoUpdater/Attributes/IsMAUSearchExtensionAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MelonAutoUpdater.Attributes
+{
+    /// <summary>
+    /// Attribute that indicates whether an assembly is a MAU Search Extension or not
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class IsMAUSearchExtensionAttribute : Attribute
+    {
+        /// <summary>
+        /// If true, assembly is a MAU Search Extension
+        /// </summary>
+        public bool Value;
+
+        /// <summary>
+        /// Creates a new instance of IsMAUSearchExtension Attribute
+        /// </summary>
+        /// <param name="value">If true, assembly is a MAU Search Extension</param>
+        public IsMAUSearchExtensionAttribute(bool value = false) => Value = value;
+    }
+}
---
+++ MelonAutoUpdater/Attributes/MAUIgnoreAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MelonAutoUpdater.Attributes
+{
+    /// <summary>
+    /// Attribute that is used to tell MAU to ignore and continue without checking and/or updating the plugin/mod
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class MAUIgnoreAttribute : Attribute
+    {
+        /// <summary>
+        /// If true, the mod/plugin will not be checked and/or updated
+        /// </summary>
+        public bool Ignore;
+
+        /// <summary>
+        /// Creates an instance of MAUIgnore Attribute
+        /// </summary>
+        /// <param name="Ignore">If true, the mod/plugin will not be checked and/or updated</param>
+        public MAUIgnoreAttribute(bool Ignore = false) => this.Ignore = Ignore;
+    }
+}
