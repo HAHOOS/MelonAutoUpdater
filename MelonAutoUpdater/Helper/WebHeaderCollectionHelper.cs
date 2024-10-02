356cbc3d0d9d8e982e9183e49942287af911d167
HAHOOS <hahoos@hahoos.pl>
2024-10-02 20:00:45 +0200

    Modify Contains method to have to be case-sensitive or not

--- MelonAutoUpdater/Helper/WebHeaderCollectionHelper.cs
+++ MelonAutoUpdater/Helper/WebHeaderCollectionHelper.cs
@@ -9,15 +9,16 @@ namespace MelonAutoUpdater.Helper
     public static class WebHeaderCollectionHelper
     {
         /// <summary>
-        /// Checks if collection contains a key
+        /// Checks if collection contains a key.
         /// </summary>
         /// <param name="collection">Collection</param>
         /// <param name="key">Key to check</param>
+        /// <param name="caseSensitive">If true, will check casing as well</param>
         /// <returns><see langword="true"/> if collection contains the specified key, otherwise <see langword="false"/></returns>
-        public static bool Contains(this WebHeaderCollection collection, string key)
+        public static bool Contains(this WebHeaderCollection collection, string key, bool caseSensitive = true)
         {
             if (collection == null) return false;
-            return collection.AllKeys.Contains(key);
+            return caseSensitive ? collection.AllKeys.Contains(key) : collection.AllKeys.Where(x => x.ToLower() == key.ToLower()).Any();
         }
     }
 }
