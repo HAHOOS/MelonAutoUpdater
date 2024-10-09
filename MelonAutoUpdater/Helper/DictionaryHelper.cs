65a46699bb7c659bd702b4e1c5fbee8c71d01b86
HAHOOS <hahoos@hahoos.pl>
2024-10-09 17:06:53 +0200

    Add Dictionary helper

---
+++ MelonAutoUpdater/Helper/DictionaryHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace MelonAutoUpdater.Helper
+{
+    /// <summary>
+    /// Helper class for <see cref="Dictionary{TKey, TValue}"/>
+    /// </summary>
+    public static class DictionaryHelper
+    {
+        /// <summary>
+        /// Checks if this <see cref="Dictionary{TKey, TValue}"/> contains provided keys
+        /// </summary>
+        /// <param name="dictionary">Dictionary to check</param>
+        /// <param name="keys">Keys to check</param>
+        /// <returns><see langword="true"/> if all found, otherwise <see langword="false"/></returns>
+        public static bool ContainsKeys<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, params TKey[] keys)
+        {
+            foreach (TKey key in keys)
+            {
+                if (!dictionary.ContainsKey(key)) return false;
+            }
+            return true;
+        }
+    }
+}
