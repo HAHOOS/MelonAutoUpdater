4784c47fd5c3d0b91a1b8a66b73b197cf118c30a
HAHOOS <hahoos@hahoos.pl>
2024-10-12 18:20:08 +0200

    Add an ID system

--- MelonAutoUpdater/Extensions/ExtensionBase.cs
+++ MelonAutoUpdater/Extensions/ExtensionBase.cs
@@ -39,6 +39,11 @@ namespace MelonAutoUpdater.Extensions
         public abstract string Author { get; }
 
         /// <summary>
+        /// ID of the Extension, this is optional and only to be able to load extensions with the same names and authors if in some case you would need, i don't know why you would need it tho.
+        /// </summary>
+        public virtual string ID { get; }
+
+        /// <summary>
         /// <see cref="Color"/> that should be displayed with the Author of the extension
         /// </summary>
         public virtual Color AuthorColor
@@ -74,7 +79,7 @@ namespace MelonAutoUpdater.Extensions
         internal void Setup()
         {
             MelonAutoUpdater.logger.DebugMsg($"Setting up logger for {Name}");
-            Logger = new MAULogger(Name);
+            Logger = new MAULogger(Name, ID);
         }
 
         #region Unload
@@ -85,7 +90,8 @@ namespace MelonAutoUpdater.Extensions
         /// </summary>
         public void Unload(bool printmsg = true)
         {
-            MelonAutoUpdater.logger.DebugMsg($"Successfully removed {LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == Author)} matches");
+            if (string.IsNullOrEmpty(ID)) LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == x.Author);
+            else LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == x.Author && x.ID == ID);
             RottenExtensions.Add(new RottenExtension(this, "The extension has been unloaded by another extension or a melon"));
             if (printmsg) MelonAutoUpdater.logger._MsgPastel($"Extension {Name.Pastel(NameColor)} has been unloaded by another extension or a melon");
         }
@@ -96,7 +102,8 @@ namespace MelonAutoUpdater.Extensions
         /// </summary>
         public void Unload(string message, bool printmsg = true)
         {
-            MelonAutoUpdater.logger.DebugMsg($"Successfully removed {LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == Author)} matches");
+            if (string.IsNullOrEmpty(ID)) LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == x.Author);
+            else LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == x.Author && x.ID == ID);
             RottenExtensions.Add(new RottenExtension(this, $"The extension has been unloaded by another extension or a melon, reason provided: {message}"));
             if (printmsg) MelonAutoUpdater.logger._MsgPastel($"Extension {Name.Pastel(NameColor)} has been unloaded by another extension or a melon with the following message: {message}");
         }
@@ -107,7 +114,8 @@ namespace MelonAutoUpdater.Extensions
         /// </summary>
         public static void Unload(SearchExtension extension, bool printmsg = true)
         {
-            MelonAutoUpdater.logger.DebugMsg($"Successfully removed {LoadedExtensions.RemoveAll(x => x.Name == extension.Name && x.Author == extension.Author)} matches");
+            if (string.IsNullOrEmpty(extension.ID)) LoadedExtensions.RemoveAll(x => x.Name == extension.Name && x.Author == x.Author);
+            else LoadedExtensions.RemoveAll(x => x.Name == extension.Name && x.Author == x.Author && x.ID == extension.ID);
             RottenExtensions.Add(new RottenExtension(extension, "The extension has been unloaded by another extension or a melon"));
             if (printmsg) MelonAutoUpdater.logger._MsgPastel($"Extension {extension.Name.Pastel(extension.NameColor)} has been unloaded by another extension or a melon");
         }
@@ -118,7 +126,8 @@ namespace MelonAutoUpdater.Extensions
         /// </summary>
         public static void Unload(SearchExtension extension, string message, bool printmsg = true)
         {
-            MelonAutoUpdater.logger.DebugMsg($"Successfully removed {LoadedExtensions.RemoveAll(x => x.Name == extension.Name && x.Author == extension.Author)} matches");
+            if (string.IsNullOrEmpty(extension.ID)) LoadedExtensions.RemoveAll(x => x.Name == extension.Name && x.Author == x.Author);
+            else LoadedExtensions.RemoveAll(x => x.Name == extension.Name && x.Author == x.Author && x.ID == extension.ID);
             RottenExtensions.Add(new RottenExtension(extension, "The extension has been unloaded by another extension or a melon"));
             if (printmsg) MelonAutoUpdater.logger._MsgPastel($"Extension {extension.Name.Pastel(extension.NameColor)} has been unloaded by another extension or a melon with the following message: {message}");
         }
@@ -129,7 +138,8 @@ namespace MelonAutoUpdater.Extensions
         /// </summary>
         internal void InternalUnload(Exception exception, bool printmsg = true)
         {
-            MelonAutoUpdater.logger.DebugMsg($"Successfully removed {LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == Author)} matches");
+            if (string.IsNullOrEmpty(ID)) LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == x.Author);
+            else LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == x.Author && x.ID == ID);
             RottenExtensions.Add(new RottenExtension(this, exception));
             if (printmsg) MelonAutoUpdater.logger._MsgPastel($"Extension {Name.Pastel(NameColor)} has been unloaded");
         }
@@ -140,7 +150,8 @@ namespace MelonAutoUpdater.Extensions
         /// </summary>
         internal void InternalUnload(Exception exception, string message, bool printmsg = true)
         {
-            MelonAutoUpdater.logger.DebugMsg($"Successfully removed {LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == Author)} matches");
+            if (string.IsNullOrEmpty(ID)) LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == x.Author);
+            else LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == x.Author && x.ID == ID);
             RottenExtensions.Add(new RottenExtension(this, exception, message));
             if (printmsg) MelonAutoUpdater.logger._MsgPastel($"Extension {Name.Pastel(NameColor)} has been unloaded");
         }
@@ -151,7 +162,8 @@ namespace MelonAutoUpdater.Extensions
         /// </summary>
         internal void InternalUnload(string message, bool printmsg = true)
         {
-            MelonAutoUpdater.logger.DebugMsg($"Successfully removed {LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == Author)} matches");
+            if (string.IsNullOrEmpty(ID)) LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == x.Author);
+            else LoadedExtensions.RemoveAll(x => x.Name == Name && x.Author == x.Author && x.ID == ID);
             RottenExtensions.Add(new RottenExtension(this, message));
             if (printmsg) MelonAutoUpdater.logger._MsgPastel($"Extension {Name.Pastel(NameColor)} has been unloaded");
         }
@@ -168,7 +180,7 @@ namespace MelonAutoUpdater.Extensions
         public MelonPreferences_Category CreateCategory(string category = "")
         {
             MelonPreferences_Category _Category = MelonPreferences.CreateCategory(!string.IsNullOrEmpty(category) ? category : Name);
-            string path = Path.Combine(Files.ExtConfigFolder, $"{Name}.cfg");
+            string path = Path.Combine(Files.ExtConfigDirectory, $"{Name}.cfg");
             _Category.SetFilePath(path);
             return _Category;
         }
@@ -219,6 +231,12 @@ namespace MelonAutoUpdater.Extensions
         /// <returns><see cref="SemVersion"/> of current MAU version</returns>
         public static SemVersion GetMAUVersion() => SemVersion.Parse(MelonAutoUpdater.Version);
 
+        /// <summary>
+        /// Get current version of ML
+        /// </summary>
+        /// <returns><see cref="SemVersion"/> of current ML version</returns>
+        public static SemVersion GetMLVersion() => MelonAutoUpdater.MLVersion;
+
         #endregion Helper
 
         #region Static Methods
@@ -232,6 +250,50 @@ namespace MelonAutoUpdater.Extensions
         /// All extensions that were unloaded due to an exception
         /// </summary>
         public static List<RottenExtension> RottenExtensions { get; internal set; } = new List<RottenExtension>();
+
+        internal static MelonInfoAttribute GetInfoFromAssembly(Assembly assembly)
+        {
+            var info = assembly.GetCustomAttribute<MelonInfoAttribute>();
+            if (info == null)
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
+                var pluginInfo = assembly.GetCustomAttribute<MelonPluginInfoAttribute>();
+#pragma warning restore CS0618 // Type or member is obsolete
+                if (pluginInfo == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    return pluginInfo;
+                }
+            }
+            else
+            {
+                return info;
+            }
+        }
+
+        internal static MelonIDAttribute GetIDFromAssembly(Assembly assembly)
+        {
+            var info = assembly.GetCustomAttribute<MelonIDAttribute>();
+            return info;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        internal static bool IsLoaded(string Name, string Author, string ID = "")
+        {
+            if (!string.IsNullOrEmpty(ID))
+            {
+                return MelonBase.RegisteredMelons.Where(x => x.Info.Name == Name && x.Info.Author == Author && x.ID == ID).Any();
+            }
+            else
+            {
+                return MelonBase.RegisteredMelons.Where(x => x.Info.Name == Name && x.Info.Author == Author).Any();
+            }
+        }
 
         /// <summary>
         /// Get all loaded extensions
@@ -248,6 +310,26 @@ namespace MelonAutoUpdater.Extensions
                 {
                     if (type.IsSubclassOf(typeof(SearchExtension)) || type.IsSubclassOf(typeof(InstallExtension)))
                     {
+                        var info = GetInfoFromAssembly(assembly);
+                        if (info == null) { MelonAutoUpdater.logger.Msg("Attempted to load an extension that is not a Melon, skipping"); return; }
+                        else
+                        {
+                            var id = GetIDFromAssembly(assembly);
+                            if (id != null)
+                            {
+                                if (!IsLoaded(info.Name, info.Author, id.ID))
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                if (!IsLoaded(info.Name, info.Author))
+                                {
+                                    continue;
+                                }
+                            }
+                        }
                         var obj = (ExtensionBase)Activator.CreateInstance(type);
                         if (obj.RequiredMAUVersion.version != null)
                         {
@@ -260,20 +342,27 @@ namespace MelonAutoUpdater.Extensions
 
                         if (obj.RequiredMLVersion.version != null)
                         {
-                            Version MelonLoaderVersion = MelonAutoUpdater.MLAssembly.GetName().Version;
-                            SemVersion MelonLoaderSemVer = new SemVersion(MelonLoaderVersion.Major, MelonLoaderVersion.Minor, MelonLoaderVersion.Build);
-
-                            if (!IsCompatible(MelonLoaderSemVer, obj.RequiredMLVersion.isMinimum, obj.RequiredMLVersion.version))
+                            if (!IsCompatible(GetMLVersion(), obj.RequiredMLVersion.isMinimum, obj.RequiredMLVersion.version))
                             {
                                 MelonAutoUpdater.logger.Msg(
                                     $"Current MAU version is not compatible with the one required by {obj.Name.Pastel(obj.NameColor)} " + $"v{obj.Version}".Pastel(MelonAutoUpdater.theme.NewVersionColor) + $" (Current is v{GetMAUVersion()}, v{obj.RequiredMLVersion.version} {(obj.RequiredMLVersion.isMinimum ? "minimally" : "specifically")})");
                             }
                         }
-
-                        if (LoadedExtensions.Find(x => x.Name == obj.Name && x.Author == obj.Author) != null)
+                        if (string.IsNullOrEmpty(obj.ID))
                         {
-                            MelonAutoUpdater.logger.Warning("Found an extension with identical Names & Author to another extension, not loading");
-                            continue;
+                            if (LoadedExtensions.Find(x => x.Name == obj.Name && x.Author == obj.Author) != null)
+                            {
+                                MelonAutoUpdater.logger.Warning("Found an extension with identical Names & Author to another extension, not loading");
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            if (LoadedExtensions.Find(x => x.Name == obj.Name && x.Author == obj.Author && x.ID == obj.ID) != null)
+                            {
+                                MelonAutoUpdater.logger.Warning("Found an extension with identical Names & Author and also ID to another extension, not loading");
+                                continue;
+                            }
                         }
                         if (type.IsSubclassOf(typeof(SearchExtension)))
                         {
