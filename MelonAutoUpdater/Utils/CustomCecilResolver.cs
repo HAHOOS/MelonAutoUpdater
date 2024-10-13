9692f0b7983edebe4e2224015c352675f105a176
HAHOOS <hahoos@hahoos.pl>
2024-10-13 16:04:08 +0200

    Add custom resolver for AssemblyDefinition to fix certain issue when checking compatibility

---
+++ MelonAutoUpdater/Utils/CustomCecilResolver.cs
@@ -0,0 +1,35 @@
+﻿using Mono.Cecil;
+using System;
+using System.Linq;
+
+namespace MelonAutoUpdater.Utils
+{
+    internal class CustomCecilResolver : BaseAssemblyResolver
+    {
+        private DefaultAssemblyResolver _defaultResolver;
+
+        public CustomCecilResolver()
+        {
+            _defaultResolver = new DefaultAssemblyResolver();
+        }
+
+        public override AssemblyDefinition Resolve(AssemblyNameReference name)
+        {
+            AssemblyDefinition assembly = null;
+            try
+            {
+                assembly = _defaultResolver.Resolve(name);
+            }
+            catch (AssemblyResolutionException ex)
+            {
+                var _assemblies = AppDomain.CurrentDomain.GetAssemblies();
+                var assemblies = _assemblies.Where(x => x.GetName().Name == name.Name);
+                if (assemblies.Any())
+                {
+                    assembly = AssemblyDefinition.ReadAssembly(assemblies.First().GetFiles().First(), new ReaderParameters() { AssemblyResolver = new CustomCecilResolver() });
+                }
+            }
+            return assembly;
+        }
+    }
+}
