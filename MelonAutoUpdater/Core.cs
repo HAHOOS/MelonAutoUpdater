12d1ce3878b981289aacab9fe14be03fef0328db
HAHOOS <hahoos@hahoos.pl>
2025-06-30 17:30:52 +0200

    Start everything from scratch

---
+++ MelonAutoUpdater/Core.cs
@@ -0,0 +1,20 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(MelonAutoUpdater.Core), "MelonAutoUpdater", "1.0.0", "HAHOOS", null)]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+
+namespace MelonAutoUpdater
+{
+    public class Core : MelonPlugin
+    {
+        public override void OnPreInitialization()
+        {
+            LoggerInstance.Msg("Pre-initialization.");
+        }
+
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
